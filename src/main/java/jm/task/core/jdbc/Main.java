e59a73d93b499b226224e6e68cd7bbfe5568d6f0
Alina Kovaleva <alinakovalyova2000@gmail.com>
2023-07-08 23:27:03 +0300

    --

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Юзер1", "123", (byte) 13);
+        userService.saveUser("Юзер2", "345", (byte) 2);
+        userService.saveUser("Юзер3", "юзер3", (byte) 12);
+        userService.saveUser("Юзер4", "юзер", (byte) 33);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "rjdf7ktdf4";
+    private static final String URL = "jdbc:mysql://localhost:3306/table";
+    private static Connection connection = null;
+    private static Util util = null;
+    private Util() {
+        try {
+            if( connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Util getInstance(){
+        if(util == null) {
+            util = new Util();
+        }
+        return util;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
